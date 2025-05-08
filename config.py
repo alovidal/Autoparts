a6acfb03bc5078a8f0078de52b7f4f17f0c47c57
alovidal <alonsini51@gmail.com>
2025-05-08 13:26:49 -0400

    Base de datos con flsk

---
+++ config.py
@@ -0,0 +1,15 @@
+import cx_Oracle
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+usuario = os.getenv("usuario")
+clave = os.getenv("clave")
+dsn = 'localhost/XEPDB1'
+
+connection = cx_Oracle.connect(
+    user=usuario,
+    password=clave,
+    dsn=dsn
+)
